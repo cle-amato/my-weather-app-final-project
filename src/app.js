abd73e05cb7148c21837f82703974f8a0d8d3758
cle-amato <cle.amato@gmail.com>
2023-11-26 15:02:51 +0000

    Added search functions to refresh current weather

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
-function searchCity(event) {
+function refreshWeather(response) {
+  let currentTemperature = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector(".current-temperature");
+  temperatureElement.innerHTML = currentTemperature;
+  let feelsLikeElement = document.querySelector("#feels-like-temperature");
+  let actualFeelsLike = Math.round(response.data.temperature.feels_like);
+  feelsLikeElement.innerHTML = actualFeelsLike;
+}
+
+function searchCity(city) {
+  let apiKey = `ca0094f431ob9cbfef2ed6ce95bt0cc7`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearch(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = cityInput.value;
+  searchCity(cityInput.value);
 }
 
 let form = document.querySelector("#search-city-form");
+form.addEventListener("submit", handleSearch);
 
-form.addEventListener("submit", searchCity);
+searchCity("Rome");
